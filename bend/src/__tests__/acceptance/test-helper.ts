74e8bf3ebdb0c9091b294dc20081c2d5393d81e9
Federico De Malmayne Duppa <fduppa@gmail.com>
2021-11-23 04:56:59 -0300

    loopback

---
+++ bend/src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,32 @@
+import {BendApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const restConfig = givenHttpServerConfig({
+    // Customize the server configuration here.
+    // Empty values (undefined, '') will be ignored by the helper.
+    //
+    // host: process.env.HOST,
+    // port: +process.env.PORT,
+  });
+
+  const app = new BendApplication({
+    rest: restConfig,
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: BendApplication;
+  client: Client;
+}
